72e0839e6e4a593d0385bfbe439d87af2b9a69a6
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-17 13:42:31 +0900

    [add] testing for failure
    - changed it to incorrect text and tested
    - failed test was successful
    - will try to test for each and differenciate between results

--- src/Main/Hello.java
+++ src/Main/Hello.java
@@ -2,8 +2,8 @@ package Main;
 
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello World! tutorial-1");
-    System.out.println("Hello World!2");
+    System.out.println("Helo World! tutorial-1");
+    System.out.println("Hello Worl!2");
   }
 
 }
