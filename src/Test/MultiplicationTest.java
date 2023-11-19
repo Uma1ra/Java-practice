44c14ff2d12ffab431d5659cb365803da7f1978c
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-20 01:38:20 +0900

    [add] 結果表示のテスト成功
    - 空白などを排除してテスト（じゃないと結果があっていても合わず）

--- src/Test/MultiplicationTest.java
+++ src/Test/MultiplicationTest.java
@@ -8,4 +8,25 @@ import static org.junit.Assert.*;
 public class MultiplicationTest {
   
   @Test
+  public void testGenerateMultiplicationTable() {
+    int number = 9;
+
+    String expectedOutput = "Multiplication for 9\n" +
+    "9 x 1 = 9\n" +
+    "9 x 2 = 18\n" +
+    "9 x 3 = 27\n" +
+    "9 x 4 = 36\n" +
+    "9 x 5 = 45\n" +
+    "9 x 6 = 54\n" +
+    "9 x 7 = 63\n" +
+    "9 x 8 = 72\n" +
+    "9 x 9 = 81\n" +
+    "9 x 10 = 90\n" +
+    "9 x 11 = 99\n" +
+    "9 x 12 = 108\n";
+
+    String actualOutput = Multiplication.generateMultiplicationTable(number);
+    assertEquals(expectedOutput.trim().replace("\r",""),
+    actualOutput.trim().replace("\r",""));
+  }
 }
