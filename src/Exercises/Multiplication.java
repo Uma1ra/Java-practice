19674c894850b52f030c6dc5705eaffe3a8a54c4
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-20 00:55:24 +0900

    [update] メソッドを変更
    - mainメソッドをテストすることが難しかった為、掛け算メソッドは外に設置し、結果をmainで呼び出す
    - StringBuilder()がパフォーマンス上best practiceとされている為、結果の表示のコードを編集

--- src/Exercises/Multiplication.java
+++ src/Exercises/Multiplication.java
@@ -3,12 +3,21 @@ package Exercises;
 public class Multiplication {
   public static void main(String[] args) {
     int number = 9;
-    System.out.println("Multiplication of " + number);
+    String multiplicationTable = generateMultiplicationTable(number);
+    System.out.println(multiplicationTable);
+  }
+
+  public static String generateMultiplicationTable(int number) {
+    StringBuilder table = new StringBuilder();
+
+    table.append("Multiplication for ").append(number).append(System.lineSeparator());
 
     for (int i = 1; i <= 12; i++) {
       int result = number * i;
 
-      System.out.println(number + " x " + i + " = " + result);
+      table.append(String.format("%d x %d = %d%n", number, i, result));
     }
+
+    return table.toString();
   }
 }
