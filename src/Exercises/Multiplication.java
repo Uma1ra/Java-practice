048f1cfa2b2379ec4b1e1286732ac7093be1175f
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-19 23:56:31 +0900

    [add] 見出し追加、結果の表示変更
    - 何をやっているのか分かるように見出しを追加
    - 結果の表示も結果のみではなく、掛け算と分かるように表示を変更

--- src/Exercises/Multiplication.java
+++ src/Exercises/Multiplication.java
@@ -3,11 +3,12 @@ package Exercises;
 public class Multiplication {
   public static void main(String[] args) {
     int number = 9;
+    System.out.println("Multiplication of " + number);
 
     for (int i = 1; i <= 12; i++) {
       int result = number * i;
 
-      System.out.println(result);
+      System.out.println(number + " x " + i + " = " + result);
     }
   }
 }
