607aec654edea3782c8a852f16cf25a14b257e5a
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-20 01:45:17 +0900

    [add] 別の数字でもテスト
    - numberを他の数字に置き換えてテストすることでメソッド内の掛け算のテストになる

--- src/Test/MultiplicationTest.java
+++ src/Test/MultiplicationTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class MultiplicationTest {
   
   @Test
-  public void testGenerateMultiplicationTable() {
+  public void testGenerateMultiplicationTableFor9() {
     int number = 9;
 
     String expectedOutput = "Multiplication for 9\n" +
@@ -24,6 +24,29 @@ public class MultiplicationTest {
     "9 x 10 = 90\n" +
     "9 x 11 = 99\n" +
     "9 x 12 = 108\n";
+
+    String actualOutput = Multiplication.generateMultiplicationTable(number);
+    assertEquals(expectedOutput.trim().replace("\r",""),
+    actualOutput.trim().replace("\r",""));
+  }
+
+  @Test
+  public void testGenerateMultiplicationTableFor5() {
+    int number = 5;
+
+    String expectedOutput = "Multiplication for 5\n" +
+    "5 x 1 = 5\n" +
+    "5 x 2 = 10\n" +
+    "5 x 3 = 15\n" +
+    "5 x 4 = 20\n" +
+    "5 x 5 = 25\n" +
+    "5 x 6 = 30\n" +
+    "5 x 7 = 35\n" +
+    "5 x 8 = 40\n" +
+    "5 x 9 = 45\n" +
+    "5 x 10 = 50\n" +
+    "5 x 11 = 55\n" +
+    "5 x 12 = 60\n";
 
     String actualOutput = Multiplication.generateMultiplicationTable(number);
     assertEquals(expectedOutput.trim().replace("\r",""),
