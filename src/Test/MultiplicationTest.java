74361acd89095779527f6258dfd59dc695d11279
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-20 01:13:00 +0900

    [update] 新しく書いたメソッド用にテストも変更
    一旦初期状態に戻した

--- src/Test/MultiplicationTest.java
+++ src/Test/MultiplicationTest.java
@@ -8,14 +8,4 @@ import static org.junit.Assert.*;
 public class MultiplicationTest {
   
   @Test
-  public void testCorrectMultiplication() {
-    int number = 5;
-
-    int[] expectedResults = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60};
-
-    for (int i = 1; i <= 12; i++) {
-      assertEquals(expectedResults[i - 1], Multiplication.main(number, i));
-    }
-  }
-
 }
