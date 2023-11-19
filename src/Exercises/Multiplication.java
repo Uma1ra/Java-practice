d1776e2465c936c02e9e98bf202bf3aa03c25c85
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-19 23:47:30 +0900

    [add] wrote a loop for multiplication and changed file name

---
+++ src/Exercises/Multiplication.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class Multiplication {
+  public static void main(String[] args) {
+    int number = 9;
+
+    for (int i = 1; i <= 12; i++) {
+      int result = number * i;
+
+      System.out.println(result);
+    }
+  }
+}
