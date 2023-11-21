d9b7cdf1e668014f86cf0582b5ed8daa378ff4c3
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-21 20:55:26 +0900

    [add] 小数点も含めた数字を足し算できるようにdoubleを使用

--- src/Exercises/UserInputAdditionInclDecimals.java
+++ src/Exercises/UserInputAdditionInclDecimals.java
@@ -1,5 +1,28 @@
 package Exercises;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UserInputAdditionInclDecimals {
-  
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+
+    try {
+      System.out.print("Input the first number: ");
+      double a = input.nextDouble();
+
+      System.out.print("Input the second number: ");
+      double b = input.nextDouble();
+
+      double result = (a + b);
+
+      System.out.println("The added result of a and b is: " + result);
+    } catch (InputMismatchException e) {
+      System.out.println("Invalid input. Please enter valid integers.");
+    } finally {
+      input.close();
+    }
+    
+  }
 }
