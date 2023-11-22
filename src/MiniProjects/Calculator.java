9231bb0eacab9f5c8e78216c211d7a9d2a0b3a7b
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-22 20:08:43 +0900

    [add] 表示メニューを作成
    スキャナーによるユーザー入力

--- src/MiniProjects/Calculator.java
+++ src/MiniProjects/Calculator.java
@@ -1,5 +1,20 @@
 package MiniProjects;
 
+import java.util.Scanner;
+
 public class Calculator {
-  
+  private Scanner scanner;
+
+  public Calculator() {
+    this.scanner = new Scanner(System.in);
+  }
+
+  public void displayMenu() {
+    System.out.println("Menu:");
+    System.out.println("1. Addition");
+    System.out.println("2. Subtraction");
+    System.out.println("3. Multiplication");
+    System.out.println("4. Division");
+    System.out.println("5. Exit");
+  }
 }
