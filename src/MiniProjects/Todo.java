2b2aa66ccb1a340842f5d8c60c936efac3e333f3
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-11 19:43:03 +0900

    [add] function for menu

--- src/MiniProjects/Todo.java
+++ src/MiniProjects/Todo.java
@@ -2,10 +2,22 @@ package MiniProjects;
 
 public class Todo {
   public static void main(String[] args) {
-    loadTasks();
+    // loadTasks();
+
+    while (true) {
+      displayMenu();
+    }
   }
 
-  private static void loadTasks() {
-    
+  // private static void loadTasks() {
+
+  // }
+
+  private static void displayMenu() {
+    System.out.println("===== To-Do List =====");
+    System.out.println("1. Display tasks");
+    System.out.println("2. Add a new task");
+    System.out.println("3. Mark a task as completed");
+    System.out.println("4. Save and Exit");
   }
 }
